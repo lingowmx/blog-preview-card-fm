5d87475ebbd6ac30e4dcad8efb34f26a87523c8a
lingowmx <clingow@hotmail.com>
2024-06-13 13:37:20 -0500

    ok with proyect

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        Yellow: 'hsl(47, 88%, 63%)',
+        White: 'hsl(0, 0%, 100%)',
+        Grey: 'hsl(0, 0%, 50%)',
+        Black: 'hsl(0, 0%, 7%)',
+      },
+      fontSize:{
+        'base': '1rem', //16px
+      },
+      fontFamily:{
+        figtree: ['Figtree', 'sans-serif'],
+      },
+      boxShadow:{
+        'customShadow': '8px 8px 0 1px Black'
+      }
+    },
   },
   plugins: [],
 };
